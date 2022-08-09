349810cd5c207779dcba2778e0405f279db8e42c
N Λ V E E N <107866323+Imnaveenjd@users.noreply.github.com>
2022-08-09 14:29:06 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,27 +24,7 @@ function Header() {
               <Image objectFit='contain' src="http://links.papareact.com/fqy"layout='fill'/>
             </Link>
         </div>
-<!--         <form className="flex flex-1 items-center space-x-2  bg-gray-100 px-3 py-1 border rounded-sm border-gray-200">
-          <SearchIcon className='h-6 w-6 text-gray-400'/>
-          <input  className='flex-1 bg-transparent outline-none' type="text" placeholder='search here'/>
-          <button type="submit" hidden/>
-        </form>
-
-        <div className='  text-gray-500 space-x-2 items-center hidden mx-5 lg:inline-flex'>
-            <SparklesIcon className='icon'/>
-            <GlobeIcon className='icon'/>
-            <VideoCameraIcon className='icon'/>
-            <hr className='h-10 border border-gray-100'></hr>
-            <ChatIcon className='icon'/>
-            <BellIcon className='icon'/>
-            <PlusIcon className='icon'/>
-            <SpeakerphoneIcon className='icon'/>
-        </div>
-
-        <div className='ml-5 flex items-center  lg:hidden'>
-          <MenuIcon className='icon'/>
-        </div> -->
-        
+     
           {session ? (
             <div 
             onClick={()=>signOut()}
@@ -64,10 +44,9 @@ function Header() {
             <ChevronDownIcon className='h-5 text-gray-400 flex-shrink-0'/>
 
             </div>
-
-          
+   
           ):(
-        <div onClick={()=>signIn()} className='flex items-center  border-gray-100 p-2  space-x-2 cursor-pointer'>
+          <div onClick={()=>signIn()} className='flex items-center  border-gray-100 p-2  space-x-2 cursor-pointer'>
 
             <div className='relative h-5 w-5 flex-shrink-0'>
               <Image objectFit='contain' src='http://links.papareact.com/23l'
@@ -77,11 +56,9 @@ function Header() {
             </div>
             <p className='text-gray-400'>Sign In</p>
 
-        </div>
+         </div>
         )}
       
-        
-
     </div>
   )
 }
