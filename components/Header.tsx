7be297e6b57aacd5d6d3881c291aca6e07b842bd
N Λ V E E N <107866323+Imnaveenjd@users.noreply.github.com>
2022-08-09 14:26:52 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -18,19 +18,13 @@ import Link from 'next/link'
 function Header() {
   const {data:session}=useSession();
   return (
-    <div className='flex  sticky top-0 z-50 bg-white px-4 py-3 shadow-sm'>
+    <div className='flex  sticky top-0 z-50 bg-white px-4 mx-auto py-4 shadow-sm'>
         <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
             <Link href='/'>
               <Image objectFit='contain' src="http://links.papareact.com/fqy"layout='fill'/>
             </Link>
         </div>
-        <div className=' flex items-center mx-7 xl:min-w-[300px]'>
-          <HomeIcon className='h-5 w-5'/>
-          <p className=' ml-2 flex-1 hidden lg:inline'>Home</p>
-          <ChevronDownIcon className='h-5 w-5'/>
-
-        </div>
-        <form className="flex flex-1 items-center space-x-2  bg-gray-100 px-3 py-1 border rounded-sm border-gray-200">
+<!--         <form className="flex flex-1 items-center space-x-2  bg-gray-100 px-3 py-1 border rounded-sm border-gray-200">
           <SearchIcon className='h-6 w-6 text-gray-400'/>
           <input  className='flex-1 bg-transparent outline-none' type="text" placeholder='search here'/>
           <button type="submit" hidden/>
@@ -49,7 +43,7 @@ function Header() {
 
         <div className='ml-5 flex items-center  lg:hidden'>
           <MenuIcon className='icon'/>
-        </div>
+        </div> -->
         
           {session ? (
             <div 
